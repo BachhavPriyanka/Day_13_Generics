c63ea84e5c142ddbb6a2a91b860418802b6f9618
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-18 17:06:37 +0530

    Finding maximum of 3 Strings

--- Generics.java
+++ Generics.java
@@ -16,8 +16,8 @@ public class Generics {
 	  } 
 
 	  public static void main(String args[]) {
-	    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-	        8.8, 7.7));
+		  System.out.printf("Maximum of %s, %s and %s is %s\n", "peach", "apple", "orange", maximum(
+			        "peach", "apple", "orange"));
 	   
 	  }
 	}
