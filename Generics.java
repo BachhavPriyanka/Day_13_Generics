ce2ac606d04ec2233181b2779a105a384af690ad
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-09-18 16:35:16 +0530

    The test data has Max number in the first position

--- Generics.java
+++ Generics.java
@@ -1,39 +1,28 @@
 package day13_Generics;
 
-import java.util.Scanner;
 
 public class Generics {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		LargestNum obj = new LargestNum();
-		System.out.println("Enter 1st number :");
-		int a = sc.nextInt();
-		System.out.println("Enter 2nd number :");
-		int b = sc.nextInt();
-		System.out.println("Enter 3rd number :");
-		int c = sc.nextInt();
-		
-		obj.large(a, b, c);	
-	}
-}
-
-class LargestNum{
-	
-	public <T extends Comparable> void large(T t1, T t2, T t3) {
-	
-		if(t1.compareTo(t2) > 0) {
-			if(t1.compareTo(t3) > 0) {
-				System.out.println(t1 + " is greatest number");
-			}else {
-				System.err.println(t3 + " is greatest number");
-			}		
-		}else {
-			if(t2.compareTo(t3) > 0) {
-				System.out.println(t2 + " is greatest number");
-			}else {
-				System.out.println(t3 + " is greatest number");
-			}
-		}
-	}
+	static int findMaximum(int arr[], int low, int high)
+    {
+       int max = arr[low];
+       int i;
+       for (i = low; i <= high; i++)
+       {
+           if (arr[i] > max)
+              max = arr[i];
+       }
+       return max;
+    }
+     
+    public static void main (String[] args)
+    {
+        int arr[] = {70, 30, 40, 50, 60, 1, 23, 20};
+        int n = arr.length;
+        System.out.println("The maximum element at 1st position is : "+
+                            findMaximum(arr, 0, n-1));
+        for(int element: arr)
+        System.out.print("Array " + element + " ");
+        
+    }
 }
